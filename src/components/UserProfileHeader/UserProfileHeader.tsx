c2c682a4f57f7b4dba6c7b281daf5f46f4fe16aa
Geovana Miranda <182geovana@gmail.com>
2025-07-04 13:15:55 -0300

    adiciona responsividade ao componente UserProfilerHeader

--- src/components/UserProfileHeader/UserProfileHeader.tsx
+++ src/components/UserProfileHeader/UserProfileHeader.tsx
@@ -8,25 +8,25 @@ const UserProfileHeader = () => {
   const photo = currentUser!.profilePhoto;
 
   return (
-    <section className="w-4xl h-auto mx-auto rounded-3xl shadow-xl bg-white relative overflow-hidden">
-      <div className=" w-4xl h-36 mx-auto absolute inset-0">
+    <section className="w-84 md:w-2xl lg:w-4xl h-auto mx-auto rounded-3xl shadow-xl bg-white relative overflow-hidden">
+      <div className="w-full h-20 md:h-28 lg:h-36 mx-auto absolute inset-0">
         <img
           src="/books.jpg"
           alt="imagem de capa do perfil do usuário"
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="mt-24 mb-6 relative z-10 w-3xl mx-auto flex">
+      <div className="w-84 md:w-xl lg:w-3xl flex flex-col items-center md:items-end md:flex-row mt-12 md:mt-18 lg:mt-24 mb-6 relative z-10  mx-auto">
         <div className="mr-4">
           <img
             src={photo}
             alt="foto de perfil do usuário"
-            className="w-32 h-32 rounded-full border border-gray-400 shadow"
+            className="w-20 h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 rounded-full border border-gray-400 shadow"
           />
         </div>
-        <div className="flex flex-col justify-end items-start  gap-1">
-          <h2 className="text-2xl font-bold">{title}</h2>
-          <p className="text-sm italic">{quote}</p>
+        <div className="flex flex-col items-center lg:justify-end md:items-start gap-1">
+          <h2 className="text-lg lg:text-2xl font-bold">{title}</h2>
+          <p className="text-center lg:text-start text-xs lg:text-sm italic">{quote}</p>
         </div>
       </div>
     </section>
