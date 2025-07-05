74ecbec01db9154b0aa660d35bbc295566d6f365
Geovana Miranda <182geovana@gmail.com>
2025-07-05 15:56:02 -0300

    adiciona responsividade ao componente NotFound

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -7,12 +7,12 @@ const NotFound = () => {
   const { currentUser } = useAuthContext();
 
   return (
-    <div className="w-3xl flex flex-col items-center">
-      <div className="flex items-center justify-around mb-10">
+    <div className="w-76 md:w-xl lg:w-3xl mx-auto flex flex-col items-center">
+      <div className="flex items-center justify-around mb-5 md:mb-10">
         <div className="flex flex-col justify-center items-center font-inter">
-          <h2 className="text-5xl font-bold">404</h2>
-          <p className="mt-2 text-3xl font-semibold">Página não encontrada</p>
-          <p className="mt-5">
+          <h2 className="text-3xl md:text-5xl font-bold">404</h2>
+          <p className="mt-2 text-xl md:text-3xl font-semibold">Página não encontrada</p>
+          <p className="mt-5 text-sm text-center md:text-base">
             Desculpe. Nós não encontramos a página que você está procurando.
           </p>
         </div>
@@ -20,7 +20,7 @@ const NotFound = () => {
       {currentUser && (
         <div>
           <button
-            className="flex items-center gap-2 px-10 py-1 w-full bg-blue-800 hover:bg-blue-600 text-white rounded-3xl transition duration-200 cursor-pointer"
+            className="flex items-center gap-2 px-10 py-1 w-full bg-blue-800 hover:bg-blue-600 text-white text-sm md:text-base rounded-3xl transition duration-200 cursor-pointer"
             onClick={() => navigate(-1)}
           >
             <IoArrowBackOutline className="text-sm" />
--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -12,29 +12,22 @@ const PageNotFound = () => {
       {currentUser ? (
         <Header />
       ) : (
-        <header className="w-4xl mx-auto py-2 my-3 px-8 flex items-center justify-between shadow bg-white rounded-4xl">
+        <header className="w-84 md:w-2xl lg:w-4xl mx-auto py-2 my-3 px-8 flex items-center justify-between shadow bg-white rounded-4xl">
           <Link to="/home">
-            <img src="/logo.png" alt="logo da minha estante" className="w-28" />
+            <img src="/logo.png" alt="logo da minha estante" className="w-22 md:w-32" />
           </Link>
 
-
-
-
-
-          <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2 md:gap-5">
             <Link to="/" className="flex items-center gap-1 hover:text-blue-800 transition duration-200">
-              <span className="font-semibold">Cadastre-se</span>
+              <span className="font-semibold text-xs md:text-lg">Cadastre-se</span>
             </Link>
             <Link
               to="/login"
               className="flex items-center gap-1 hover:text-blue-800 transition duration-200"
             >
-              <span className="font-semibold">Login</span>
+              <span className="font-semibold text-xs md:text-lg">Login</span>
             </Link>
-          </div>
-
-
-          
+          </div>          
         </header>
       )}
       <Section>
